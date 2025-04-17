8d62be4e202e2de3587feef26984eacbdc4ba9ed
manojrajpoot-r <manojtechsaga08@gmail.com>
2025-04-17 12:50:58 +0530

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CDN Example</title>
+    <title>Simple Project</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-
+<h1> Simple project create</h1>
 <div class="container">
     <form id="myForm">
         <div class="form-group">
